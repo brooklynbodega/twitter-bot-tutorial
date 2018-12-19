613f5fbff370ce0d19e5b2ff6d78f6d4f34e9276
Raul Montero <raul.amontero@gmail.com>
2018-12-19 00:04:24 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+
+module.exports = {
+  consumer_key: '1h5NLE1P9fxig4dtxUv3DC9OQ',
+  consumer_secret: '8rTjJMyahA4fQvHevWiPPR0vjW3FggKMoofyEnnV1vyYvS7hK9',
+  access_token_key: '1075138315068891136-h9btRoMnwlL44ymqBzOgxMRUeEKpDR',
+  access_token_secret: 'v6SqMoDEDGBJE3YnqQsDFbBc03t1XAeDsUCrFj3iAf3p6'
+}
